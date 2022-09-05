ba03e94e6082e0acdf5dcdc961f9bbd83ae071b4
Thiliban <thilip2017@gmail.com>
2022-09-05 09:57:25 +0530

    Initial commit

---
+++ src/Book.js
@@ -0,0 +1,37 @@
+// import {books} from "./books";
+
+const Book = ({ img, title, author }) => {
+  const clickHandler = (e) => {
+    console.log(e);
+    console.log(e.target);
+  };
+  const complexExample = (author) => {
+    console.log(author);
+  };
+  return (
+    <article
+      className="book"
+      onMouseOver={() => {
+        console.log(title);
+      }}
+    >
+      <img src={img} alt="" />
+      <h1
+        onClick={() => {
+          console.log(title);
+        }}
+      >
+        {title}
+      </h1>
+      <h4>{author}</h4>
+      <button type="button" onClick={clickHandler}>
+        reference example
+      </button>
+      <button type="button" onClick={() => complexExample(author)}>
+        more complex example
+      </button>
+    </article>
+  );
+};
+
+export default Book;
---
+++ src/books.js
@@ -0,0 +1,20 @@
+export const books = [
+  {
+    id: 1,
+    img: "https://images-eu.ssl-images-amazon.com/images/W/WEBP_402378-T2/images/I/51MVWaLLR7L._AC_SX368_.jpg",
+    title: "Valli A Novel",
+    author: "Sheela Tomy ",
+  },
+  {
+    id: 2,
+    img: "https://m.media-amazon.com/images/W/WEBP_402378-T2/images/I/8144Vic9C5L._AC_UY327_FMwebp_QL65_.jpg",
+    title: "Animals Tales From Panchtantra",
+    author: "Wonder House Books",
+  },
+  {
+    id: 3,
+    img: "https://images-eu.ssl-images-amazon.com/images/W/WEBP_402378-T2/images/I/41LD2VhDn8L._AC_SX184_.jpg",
+    title: "The Hero Of Tiger Hill",
+    author: "Yogendra Singh Yadav",
+  },
+];
